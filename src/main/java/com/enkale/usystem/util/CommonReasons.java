0e2d807a004b313d9d6c8671e44f2473646f3a9a
Leibniz H. Berihuete <leibniz.berihuete@gmail.com>
2020-12-26 08:11:10 -0400

    Adds INCORRECT_EMAIL_OR_PASSWORD to CommonReasons

--- src/main/java/com/enkale/usystem/util/CommonReasons.java
+++ src/main/java/com/enkale/usystem/util/CommonReasons.java
@@ -3,4 +3,5 @@ package com.enkale.usystem.util;
 public class CommonReasons {
     public static final String REQUEST_IS_NULL = "Request is null";
     public static final String FIELDS_ARE_EMPTY_OR_NULL = "Field(s) are either empty or null";
+    public static final String INCORRECT_EMAIL_OR_PASSWORD = "Incorrect email or password";
 }
